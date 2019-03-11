d0fa9b9cb370032cf4ade82c24f13cd65e36e741
info@appertunity.net <info@appertunity.net>
2019-03-11 17:39:06 +0500

    LMGO portal.

--- application/views/common/page.php
+++ application/views/common/page.php
@@ -36,9 +36,25 @@
          style="visibility: hidden; animation-duration: 1s; animation-delay: 0.3s; animation-name: none;">
     <div class="container">
         <div class="w3eden" >
+            <?php if (strtolower($type) != "home" ) { ?>
+            <div class="widget widget_main_title" >
+                <h2><?php echo $page_content->title ?></h2>
+            </div>
+            <?php } ?>
+
+            <?php if ($page_content->image && $page_content->show_image) {
+                echo '<img src="' . base_url() . '/uploads/pages/' . $page_content->image . '" alt="' . $page_content->tag . '">';
+            } ?>
             <?php if ($page_content->long_desc) {
                 echo html_entity_decode( str_replace('&quot;','', $page_content->long_desc));
             } ?>
+
+            <?php if ($page_content->pdf) { ?>
+                <div id="dwn-btn-here" class="col-md-12" style="padding-bottom: 15px;">
+                    <a href="<?php echo base_url() ?>uploads/pages/pdf/<?php echo $page_content->pdf ?>" target="_blank" class="btn"><i class="fas fa-external-link-alt"></i> Open</a>
+                    <a href="<?php echo base_url() ?>uploads/pages/pdf/<?php echo $page_content->pdf ?>" download='href="<?php echo base_url() ?>uploads/pages/pdf/<?php echo $page_content->pdf ?>"' class="btn"><i class="fa fa-download"></i> Download</a>
+                </div>
+            <?php }?>
 
         </div>
     </div>
