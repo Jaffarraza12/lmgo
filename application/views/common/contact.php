832ee69f4e371466e7816c5db2f927c78289742f
jaffar raza <jaffaraza@gmail.com>
2019-03-28 23:39:15 -0700

    New Changes

--- application/views/common/contact.php
+++ application/views/common/contact.php
@@ -1,5 +1,67 @@
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<style>
+    /*
+** Style Simple Ecommerce Theme for Bootstrap 4
+** Created by T-PHP https://t-php.fr/43-theme-ecommerce-bootstrap-4.html
+*/
+    .bloc_left_price {
+        color: #c01508;
+        text-align: center;
+        font-weight: bold;
+        font-size: 150%;
+    }
+    .category_block li:hover {
+        background-color: #007bff;
+    }
+    .category_block li:hover a {
+        color: #ffffff;
+    }
+    .category_block li a {
+        color: #343a40;
+    }
+    .add_to_cart_block .price {
+        color: #c01508;
+        text-align: center;
+        font-weight: bold;
+        font-size: 200%;
+        margin-bottom: 0;
+    }
+    .add_to_cart_block .price_discounted {
+        color: #343a40;
+        text-align: center;
+        text-decoration: line-through;
+        font-size: 140%;
+    }
+    .product_rassurance {
+        padding: 10px;
+        margin-top: 15px;
+        background: #ffffff;
+        border: 1px solid #6c757d;
+        color: #6c757d;
+    }
+    .product_rassurance .list-inline {
+        margin-bottom: 0;
+        text-transform: uppercase;
+        text-align: center;
+    }
+    .product_rassurance .list-inline li:hover {
+        color: #343a40;
+    }
+    .reviews_product .fa-star {
+        color: gold;
+    }
+    .pagination {
+        margin-top: 20px;
+    }
+    footer {
+        background: #343a40;
+        padding: 40px;
+    }
+    footer a {
+        color: #f8f9fa!important
+    }
 
+</style>
 
 <div class="container">
     <div class="row w3eden">
@@ -28,7 +90,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4    ">
             <div class="card bg-light mb-3">
                 <div class="card-header bg-success text-white text-uppercase"><i class="fa fa-home"></i> Address</div>
                 <div class="card-body">
