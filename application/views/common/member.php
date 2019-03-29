d86c07307a935e17e7b082c0ec8ceb92a5b91c90
jaffar raza <jaffaraza@gmail.com>
2019-03-29 04:42:21 -0700

    yo

--- application/views/common/member.php
+++ application/views/common/member.php
@@ -41,7 +41,7 @@
                                 <div class="membership-header"></div>
                                 <div class="membership-content">
                                 <div class="detail"><a >Name :<?php echo $member->name ?></a><br/><a >Program :<?php echo $member->program ?></a></div>
-                                <div class="detail">  <img width="300" src="<?php echo base_url().'uploads/certificate/'.$member->image ?>" /></div>
+                                <div class="detail">  <img width="150" src="<?php echo base_url().'uploads/certificate/'.$member->image ?>" /></div>
                                </div>
                                 <div class="membership-footer">
                                     <div class="copyright">
