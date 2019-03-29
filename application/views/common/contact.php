25db438feac2fdd422f02736904262ff237bc8bf
jaffar raza <jaffaraza@gmail.com>
2019-03-28 23:35:39 -0700

    New Changes

--- application/views/common/contact.php
+++ application/views/common/contact.php
@@ -1,75 +1,9 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-
-<!------ Include the above in your HEAD tag ---------->
-
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<style>
-    /*
-** Style Simple Ecommerce Theme for Bootstrap 4
-** Created by T-PHP https://t-php.fr/43-theme-ecommerce-bootstrap-4.html
-*/
-    .bloc_left_price {
-        color: #c01508;
-        text-align: center;
-        font-weight: bold;
-        font-size: 150%;
-    }
-    .category_block li:hover {
-        background-color: #007bff;
-    }
-    .category_block li:hover a {
-        color: #ffffff;
-    }
-    .category_block li a {
-        color: #343a40;
-    }
-    .add_to_cart_block .price {
-        color: #c01508;
-        text-align: center;
-        font-weight: bold;
-        font-size: 200%;
-        margin-bottom: 0;
-    }
-    .add_to_cart_block .price_discounted {
-        color: #343a40;
-        text-align: center;
-        text-decoration: line-through;
-        font-size: 140%;
-    }
-    .product_rassurance {
-        padding: 10px;
-        margin-top: 15px;
-        background: #ffffff;
-        border: 1px solid #6c757d;
-        color: #6c757d;
-    }
-    .product_rassurance .list-inline {
-        margin-bottom: 0;
-        text-transform: uppercase;
-        text-align: center;
-    }
-    .product_rassurance .list-inline li:hover {
-        color: #343a40;
-    }
-    .reviews_product .fa-star {
-        color: gold;
-    }
-    .pagination {
-        margin-top: 20px;
-    }
-    footer {
-        background: #343a40;
-        padding: 40px;
-    }
-    footer a {
-        color: #f8f9fa!important
-    }
 
-</style>
 
 <div class="container">
     <div class="row">
-        <div class="col">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-header bg-primary text-white"><i class="fa fa-envelope"></i> Contact us.
                 </div>
@@ -94,7 +28,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-12 col-sm-4">
+        <div class="col-md-4">
             <div class="card bg-light mb-3">
                 <div class="card-header bg-success text-white text-uppercase"><i class="fa fa-home"></i> Address</div>
                 <div class="card-body">
