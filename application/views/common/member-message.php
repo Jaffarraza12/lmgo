8f9125656eaabf5e8f82632fe8cfa74898406a90
jaffar raza <jaffaraza@gmail.com>
2019-03-29 05:39:34 -0700

    yo

---
+++ application/views/common/member-message.php
@@ -0,0 +1,113 @@
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<style>
+    /*
+** Style Simple Ecommerce Theme for Bootstrap 4
+** Created by T-PHP https://t-php.fr/43-theme-ecommerce-bootstrap-4.html
+*/
+    .bloc_left_price {
+        color: #c01508;
+        text-align: center;
+        font-weight: bold;
+        font-size: 150%;
+    }
+    .category_block li:hover {
+        background-color: #007bff;
+    }
+
+    .w3eden .card .card-body{
+        display: block;
+        border: 1px solid #EAEAEA;
+    }
+
+
+    .w3eden .btn-primary{
+        background:#247104 !important; ;
+    }
+
+
+    .bg-primary,.bg-success{
+        padding:5px 10px;
+    }
+    .category_block li:hover a {
+        color: #ffffff;
+    }
+
+
+    .category_block li a {
+        color: #343a40;
+    }
+    .add_to_cart_block .price {
+        color: #c01508;
+        text-align: center;
+        font-weight: bold;
+        font-size: 200%;
+        margin-bottom: 0;
+    }
+    .add_to_cart_block .price_discounted {
+        color: #343a40;
+        text-align: center;
+        text-decoration: line-through;
+        font-size: 140%;
+    }
+    .product_rassurance {
+        padding: 10px;
+        margin-top: 15px;
+        background: #ffffff;
+        border: 1px solid #6c757d;
+        color: #6c757d;
+    }
+    .product_rassurance .list-inline {
+        margin-bottom: 0;
+        text-transform: uppercase;
+        text-align: center;
+    }
+    .product_rassurance .list-inline li:hover {
+        color: #343a40;
+    }
+    .reviews_product .fa-star {
+        color: gold;
+    }
+    .pagination {
+        margin-top: 20px;
+    }
+    footer {
+        background: #343a40;
+        padding: 40px;
+    }
+    footer a {
+        color: #f8f9fa!important
+    }
+
+</style>
+
+<div class="container">
+    <div class="row w3eden">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header bg-primary text-white"><i class="fa fa-envelope"></i> Contact us.
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Member ID</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter ID" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email address</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" required>
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="message">Message</label>
+                            <textarea class="form-control" id="message" rows="6" required></textarea>
+                        </div>
+                        <div class="mx-auto">
+                            <button type="submit" class="btn btn-primary text-right bg-success">Send</button></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
